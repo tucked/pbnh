70f5bc0c9596bccd8240ed9f09883b018cfbd959
David Tucker <david.tucker@emc.com>
2023-02-17 12:24:47 -0800

    Fix paste_psql_test.py and move it to tests/

--- tests/paste_psql_test.py
+++ tests/paste_psql_test.py
@@ -5,27 +5,37 @@ from datetime import datetime
 from pbnh.db.createdb import CreateDB
 from pbnh.db import paste
 
-dialect = "postgresql"
-dbname = "testdb"
+
+CONFIG = {
+    "database": {
+        "dbname": "pastedb",
+        "dialect": "postgresql",
+        "driver": None,
+        "host": "db",
+        "password": "postgres",
+        "port": 5432,
+        "username": "postgres",
+    },
+}
 
 
 class TestPaster(unittest.TestCase):
     def setUp(self):
-        self.newdb = CreateDB(dialect=dialect, dbname=dbname)
+        self.newdb = CreateDB(**CONFIG["database"])
         self.newdb.create()
 
     def tearDown(self):
         self.newdb.delete()
 
     def test_create_new(self):
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             created = p.create(b"This is a test paste")
         self.assertEqual(
             created, {"id": 1, "hashid": "f872a542a8289d2273f6cb455198e06126f4ec30"}
         )
 
     def test_create_dupe(self):
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             created = p.create(b"This is a test paste")
             created = p.create(b"This is a test paste")
         self.assertEqual(
@@ -34,7 +44,7 @@ class TestPaster(unittest.TestCase):
 
     def test_query_id(self):
         timestamp = datetime.now()
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             p.create(b"This is a test paste", timestamp=timestamp)
             lookup = p.query(id=1)
         self.assertEqual(
@@ -52,7 +62,7 @@ class TestPaster(unittest.TestCase):
 
     def test_query_hash(self):
         timestamp = datetime.now()
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             p.create(b"This is a test paste", timestamp=timestamp)
             lookup = p.query(hashid="f872a542a8289d2273f6cb455198e06126f4ec30")
         self.assertEqual(
@@ -69,26 +79,26 @@ class TestPaster(unittest.TestCase):
         )
 
     def test_query_disaster(self):
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             p.create(b"This is a test paste")
             with self.assertRaises(ValueError):
                 p.query(id="f872a542a8289d2273f6cb455198e06126f4ec30")
 
     def test_query_none(self):
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             p.create(b"This is a test paste")
             lookup = p.query()
         self.assertEqual(lookup, None)
 
     def test_delete_id(self):
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             p.create(b"This is a test paste")
             p.delete(id=1)
             lookup = p.query(id=1)
         self.assertEqual(lookup, None)
 
     def test_delete_hash(self):
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             p.create(b"This is a test paste")
             p.delete(hashid="f872a542a8289d2273f6cb455198e06126f4ec30")
             lookup = p.query(id=1)
@@ -96,7 +106,7 @@ class TestPaster(unittest.TestCase):
 
     def test_delete_none(self):
         timestamp = datetime.now()
-        with paste.Paster(dialect=dialect, dbname=dbname) as p:
+        with paste.Paster(**CONFIG["database"]) as p:
             p.create(b"This is a test paste", timestamp=timestamp)
             p.delete()
             lookup = p.query(id=1)
