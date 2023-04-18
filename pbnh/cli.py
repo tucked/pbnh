dce3204aa101d927164809969a1c1ab7deb1144a
David Tucker <david.tucker@emc.com>
2023-04-18 13:52:53 -0700

    Add `paste info` to the CLI
    Enable admins to get paste info without having to access the DB directly.

--- pbnh/cli.py
+++ pbnh/cli.py
@@ -1,3 +1,5 @@
+import hashlib
+
 import click
 from flask import Blueprint
 
@@ -15,3 +17,46 @@ def db() -> None:
 def init() -> None:
     pbnh.db.init_db()
     click.echo("initialized the database successfully")
+
+
+@blueprint.cli.group()  # type: ignore
+@click.pass_context
+def paste(ctx: click.Context) -> None:
+    ctx.obj.data["paster"] = ctx.with_resource(pbnh.db.paster_context())
+
+
+@paste.command()  # type: ignore
+@click.option(
+    "--show-data/--no-show-data",
+    help="whether to show paste data",
+    default=False,
+    show_default=True,
+)
+@click.argument("hashids", type=str, nargs=-1)
+@click.pass_context
+def info(ctx: click.Context, show_data: bool, hashids: tuple[str]) -> None:
+    """Get info on pastes."""
+    for multiple_hashids, hashid in enumerate(hashids):
+        if multiple_hashids:
+            click.echo("=" * 80)
+        paste = ctx.obj.data["paster"].query(hashid=hashid)
+        if paste is None:
+            click.echo(f"{hashid} not found")
+            continue
+        value = paste["hashid"]
+        hashid = hashlib.sha1(paste["data"], usedforsecurity=False).hexdigest()
+        if value != hashid:
+            value += click.style(
+                f" (WARNING: expected {hashid})",
+                fg="yellow",
+            )
+        for column, value in (
+            [("hashid", value)]
+            + [
+                (column, value)
+                for column, value in paste.items()
+                if column not in {"data", "hashid"}
+            ]
+            + [("data", paste["data"] if show_data else f"({len(value)} bytes)")]
+        ):
+            click.echo(f"{column + ':':>15} {value}")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,20 @@
+import contextlib
+import unittest.mock
+
 import pytest
+
+
+def fake_paster_context_factory(hashid, data):
+    @contextlib.contextmanager
+    def fake_paster_context():
+        mock = unittest.mock.Mock()
+        mock.query.return_value = {
+            "hashid": hashid,
+            "data": data,
+        }
+        yield mock
+
+    return fake_paster_context
 
 
 @pytest.fixture
@@ -18,3 +34,36 @@ def test_cli_db_init(test_cli_runner, monkeypatch):
     result = test_cli_runner.invoke(args=["db", "init"])
     assert init_db_called
     assert "initialized" in result.output
+
+
+def test_cli_paste_info(test_cli_runner, monkeypatch):
+    """Pastes can be looked up from the CLI."""
+    data = b"Example Data"
+    hashid = "3eb000f2018951656c8c27c5dc2a37445d029128"
+    monkeypatch.setattr(
+        "pbnh.db.paster_context", fake_paster_context_factory(hashid, data)
+    )
+    result = test_cli_runner.invoke(args=["paste", "info", hashid, hashid])
+    assert hashid in result.output
+    assert "data" in result.output
+
+
+def test_cli_paste_info_not_found(app, test_cli_runner):
+    """Looking up a nonexistent paste is handled gracefully."""
+    hashid = "abc123"
+    with app.app_context():
+        result = test_cli_runner.invoke(args=["paste", "info", hashid])
+    assert hashid in result.output
+    assert "not found" in result.output
+
+
+def test_cli_paste_info_hash_mismatch(test_cli_runner, monkeypatch):
+    """A warning is emitted if a paste hashid doesn't match the data."""
+    data = b"Example Data"
+    hashid = "hash-does-not-match"
+    monkeypatch.setattr(
+        "pbnh.db.paster_context", fake_paster_context_factory(hashid, data)
+    )
+    result = test_cli_runner.invoke(args=["paste", "info", hashid])
+    assert hashid in result.output
+    assert "WARNING" in result.output
