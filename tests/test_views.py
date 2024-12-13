ba20140265fd0df069c6bc8df84d414ea5d3a970
David Tucker <david.tucker@emc.com>
2024-12-13 20:42:29 +0000

    Fix test_get_no_extension tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -299,22 +299,32 @@ def test_post_collision(content_key, test_client):
     assert response.status_code == 409
 
 
-@pytest.fixture(params=[".", "/"])
-def delimiter(request):
-    return request.param
-
-
-def test_get_no_extension(content_key, test_client, delimiter):
+def test_get_no_extension_dot(content_key, test_client):
     response = test_client.post("/", data={content_key: "abc"})
     hashid = response.json["hashid"]
-    response = test_client.get(f"/{hashid}{delimiter}")
-    response.status_code == 301
-    response.location == f"/{hashid}{delimiter}txt"
+    response = test_client.get(f"/{hashid}.")
+    assert response.status_code == 301
+    assert response.location == f"/{hashid}.txt"
 
 
-def test_get_no_extension_unguessable(content_key, test_client, delimiter):
+def test_get_no_extension_slash(content_key, test_client):
+    response = test_client.post("/", data={content_key: "abc"})
+    hashid = response.json["hashid"]
+    response = test_client.get(f"/{hashid}/")
+    assert response.status_code == 301
+    assert response.location == f"/{hashid}/text"
+
+
+def test_get_no_extension_unguessable_dot(content_key, test_client):
     response = test_client.post("/", data={content_key: "abc", "mime": "fo/shizzle"})
     hashid = response.json["hashid"]
-    response = test_client.get(f"/{hashid}{delimiter}")
-    response.status_code == 302
-    response.location == f"/{hashid}"
+    response = test_client.get(f"/{hashid}.")
+    assert response.status_code == 422
+
+
+def test_get_no_extension_unguessable_slash(content_key, test_client):
+    response = test_client.post("/", data={content_key: "abc", "mime": "fo/shizzle"})
+    hashid = response.json["hashid"]
+    response = test_client.get(f"/{hashid}/")
+    assert response.status_code == 301
+    assert response.location == f"/{hashid}/raw"
